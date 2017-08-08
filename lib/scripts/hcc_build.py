edbf8e1b0e02b0c0f5a0a5b6ae0ab011182ef72f
Sytchenko <vsytchen@amd.com>
2017-08-08 15:06:22 -0400

    improved build script

--- lib/scripts/hcc_build.py
+++ lib/scripts/hcc_build.py
@@ -9,17 +9,19 @@ debug = False
 clang = "clang++"
 hcc = "hcc" 
 
-def compile_mcwamp():
-    command = [clang,
-        "-DWIN32=1",
+args = ["-DWIN32=1",
         "-D_WIN32=1",
         "-D_WIN64=1",
+        "-D_M_X64=100",
+        "-D_M_AMD64=100",
         "-D_MT=1",
         "-D_DLL=1",
         "-D_CONSOLE=1",
+        "-D_WCHAR_T_DEFINED=1",
+        "-D_NATIVE_WCHAR_T_DEFINED=1",
+        "-D_CPPUNWIND=1",
         "-D_USE_MATH_DEFINES=1",
         "-D_CRT_SECURE_NO_WARNINGS=1",
-        "-I" + bin_dir + "/../../../include",
         "-IC:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/include",
         "-IC:/Program Files (x86)/Windows Kits/10/include/10.0.10240.0/ucrt",
         "-IC:/Program Files (x86)/Windows Kits/NETFXSDK/4.6.1/include/um",
@@ -65,91 +67,44 @@ def compile_mcwamp():
         "-Wno-ignored-pragma-intrinsic",
         "-Wno-expansion-to-defined",
         "-Wno-int-to-void-pointer-cast",
-        "-c",
         "-v"]
-    
+
+def compile_mcwamp():
+    command = [clang]
+    command += ["-I" + bin_dir + "/../../../include"]
+    command += args
+
     if debug:
         command += ["-D_DEBUG=1",
-            "-gcodeview"]
+                    "-gcodeview"]
     else:
         command += ["-DNDEBUG=1"]
 
-    command += ["-o",
-        bin_dir + "/../lib/mcwamp.obj",
-        bin_dir + "/../../../lib/mcwamp.cpp"]
+    command += ["-c",
+                "-o",
+                bin_dir + "/../lib/mcwamp.obj",
+                bin_dir + "/../../../lib/mcwamp.cpp"]
 
     check_call(command)
 
 def compile_source():
-    command = [clang,
-        "-DWIN32=1",
-        "-D_WIN32=1",
-        "-D_WIN64=1",
-        "-D_MT=1",
-        "-D_DLL=1",
-        "-D_CONSOLE=1",
-        "-D_USE_MATH_DEFINES=1",
-        "-D_CRT_SECURE_NO_WARNINGS=1",
-        "-I" + bin_dir + "/../../../include",
-        "-IC:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/include",
-        "-IC:/Program Files (x86)/Windows Kits/10/include/10.0.10240.0/ucrt",
-        "-IC:/Program Files (x86)/Windows Kits/NETFXSDK/4.6.1/include/um",
-        "-IC:/Program Files (x86)/Windows Kits/8.1/Include/um",
-        "-IC:/Program Files (x86)/Windows Kits/8.1/Include/shared",
-        "-fms-compatibility-version=19.0.24215",
-        "-fms-compatibility",
-        "-fms-extensions",
-        "-fdelayed-template-parsing",
-        "-fdeclspec",
-        "-Xclang",
-        "-flto-visibility-public-std",
-        "-Xclang",
-        "-fms-volatile",
-        "-Xclang",
-        "-fdiagnostics-format",
-        "-Xclang",
-        "msvc",
-        "-Xclang",
-        "-fexternc-nounwind",
-        "-Xclang",
-        "-relaxed-aliasing",
-        "-Xclang",
-        "-mrelocation-model",
-        "-Xclang",
-        "pic",
-        "-Xclang",
-        "-pic-level",
-        "-Xclang",
-        "2",
-        "-Xclang",
-        "-munwind-tables",
-        "-Xclang",
-        "-momit-leaf-frame-pointer",
-        "-mincremental-linker-compatible",
-        "-hc",
-        "-std=c++amp",
-        "-Wno-ignored-attributes",
-        "-Wno-microsoft-template",
-        "-Wno-duplicate-decl-specifier",
-        "-Wno-logical-op-parentheses",
-        "-Wno-dangling-else",
-        "-Wno-nonportable-include-path",
-        "-Wno-ignored-pragma-intrinsic",
-        "-Wno-expansion-to-defined",
-        "-Wno-int-to-void-pointer-cast",
-        "-v"]
-    
+    command = [hcc]
+    command += ["-I" + bin_dir + "/../../../include"]
+    command += args
+
     if debug:
         command += ["-D_DEBUG=1",
-            "-gcodeview"]
+                    "-gcodeview"]
     else:
         command += ["-DNDEBUG=1"]
 
-    command += ["-o",
-        src_file[:-4] + ".exe",
-        src_file]
+    command += ["-hc",
+                "-o",
+                src_file[:-4] + ".exe",
+                src_file]
 
     check_call(command)
+
 if __name__ == "__main__":
     bin_dir = os.path.dirname(argv[0])
 
